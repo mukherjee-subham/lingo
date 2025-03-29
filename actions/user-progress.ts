2d39421a66a26ad60087e0fdc2bb4b196ede1d7c
Subham Mukhopadhyay <coolestguy.mukherjee@gmail.com>
2025-03-29 21:57:33 +0530

    implement courses page, user progress, db scripts and schema, actions

---
+++ actions/user-progress.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import db from "@/db/drizzle";
+import { getCourseById, getUserProgress } from "@/db/queries";
+import { userProgress } from "@/db/schema";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const upsertUserProgress = async (courseId: number) => {
+  const { userId } = await auth();
+  const user = await currentUser();
+
+  if (!userId || !user) {
+      console.log("Unauth")
+      throw new Error("Unauthorized");
+  }
+
+  const course = await getCourseById(courseId);
+
+  if (!course) {
+        console.log("Course not found")
+      throw new Error("Course not found");
+} 
+
+  // if (!course.unit.length || course.unit[0].lessons.length)
+  //     throw new Error("Course is empty")
+
+  const existingUserProgress = await getUserProgress();
+
+  // update the active course
+  if (existingUserProgress) {
+    await db.update(userProgress).set({
+      activeCourseId: courseId,
+      userName: user.firstName || "User",
+      userImageSrc: user.imageUrl || "/mascot.svg",
+    });
+    revalidatePath("/courses");
+    revalidatePath("/learn");
+    redirect("/learn");
+  }
+
+  await db.insert(userProgress).values({
+    userId,
+    activeCourseId: courseId,
+    userName: user.firstName || "User",
+    userImageSrc: user.imageUrl || "/mascot.svg",
+  });
+
+  revalidatePath("/courses");
+  revalidatePath("/learn");
+  redirect("/learn");
+};
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses, userProgress } from "@/db/schema";
 
 type Props = {
-    activeCourse: { imgSrc: string; title: string };
+    activeCourse: typeof courses.$inferSelect;
     hearts: number;
     points: number;
     hasActiveSubscription: boolean;
@@ -15,7 +16,7 @@ export const UserProgress = ({activeCourse, hearts, points, hasActiveSubscriptio
         <div className="flex items-center justify-between gap-x-2 w-full">
             <Link href="/courses">
                 <Button variant="ghost">
-                    <Image src={activeCourse.imgSrc} alt={activeCourse.title} className="rounded-md border" width={32} height={32}/>
+                    <Image src={activeCourse.imageSrc} alt={activeCourse.title} className="rounded-md border" width={32} height={32}/>
                 </Button>
             </Link>
             <Link href="/shop">
